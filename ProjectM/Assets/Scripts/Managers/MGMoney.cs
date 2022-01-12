fb71106022b20171e75675e4ffd62f8d797e64d2
HanUseArch <wooyeophan@gmail.com>
2022-01-12 12:56:14 +0900

    event added

--- ProjectM/Assets/Scripts/Managers/MGMoney.cs
+++ ProjectM/Assets/Scripts/Managers/MGMoney.cs
@@ -1,14 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MGMoney : MonoSingleton<MGMoney>
 {
+    /// <summary>
+    /// 돈 변경 시 호출됨. (int = addedAmount)
+    /// </summary>
+    public event Action<int> OnMoneyChanged;
+
     private int _money = 0;
     public int Money { get { return _money; } }
+
+    private void Awake()
+    {
+        OnMoneyChanged += (e) => { };
+    }
 
     private void AddMoney(int amount)
     {
         _money += amount;
+
+        OnMoneyChanged(amount);
     }
 }
