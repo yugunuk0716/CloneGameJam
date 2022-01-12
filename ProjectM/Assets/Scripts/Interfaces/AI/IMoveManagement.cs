37c95ef5d891820674acc6c62a82132e0c970d22
HanUseArch <wooyeophan@gmail.com>
2022-01-12 16:04:42 +0900

    IMoveManagement

--- ProjectM/Assets/Scripts/Interfaces/AI/IMoveManagement.cs
+++ ProjectM/Assets/Scripts/Interfaces/AI/IMoveManagement.cs
@@ -6,8 +6,5 @@ using UnityEngine.AI;
 public interface IMoveManagement
 {
     public int CurrentDestIdx { get; }
-    public float DefaultSpeed { get; }
-
     public float GetRemainDistance();
-    public void SetSpeed(float multipy, float duration);
 }
