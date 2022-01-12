ac7d3d62992937e8f59c14cdad8c2fdcb9429fb3
HanUseArch <wooyeophan@gmail.com>
2022-01-12 10:10:20 +0900

    Global 수정

--- ProjectM/Assets/Scripts/Common/Global.cs
+++ ProjectM/Assets/Scripts/Common/Global.cs
@@ -22,11 +22,13 @@ public enum ePrefabs
     MANAGERS = 2000,
     MGPool,
     MGGame,
+    MGTimeScale,
     UI = 3000,
     UIRoot,
     UIRootLoading,
     UIRootTitle,
     UIRootGame,
+    UITimeScale,
 }
 
 public enum eSceneName
